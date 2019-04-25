50cb6ed7588d0182b71bb1d2f64de14e1d6898c7
Arbi Watt <arbi.watt@uon.edu.au>
2019-04-25 07:45:52 +0000

    added a basic encryption for substitution, onto the formatting.

--- main.c
+++ main.c
@@ -1,92 +1,102 @@
 #include <stdio.h>
- 
-int main()
+int main () 
 {
-	char message[100], ch;
-	int i=0, key;
-	int operation, cypher;
-	
-	printf("cypher list\n");
-	printf("1. ceaser cypher\n");
-	printf("2. substitution cypher \n");
-	
-	printf("choose cypher:");
-	scanf("%d", &cypher);
-
-	if(cypher = 1){
-	    printf("1. encrypt message\n");
-	    printf("2. decrypt message\n");
-	    printf("choose program: "); 
-	    scanf("%d", &operation) ;  
-	    
-	switch (operation){           
-	
-	case 1 :
-	
-	printf("Enter a message to encrypt: ");
-	scanf("%s", message);
-	
-	  while (message[i] != '\0') {
-      if (message[i] >= 'a' && message[i] <= 'z') 
-         message[i] = message[i] - 32;
-      
-      i++;
-	      
-	  }
-      
-	printf("Enter key: ");
-	scanf("%d", &key);
-	
-	for(i = 0; message[i] != '\0'; ++i){
-		ch = message[i];
-		
-		if(ch >= 'A' && ch <= 'Z'){
-			ch = ch + key;
-			
-			if(ch > 'Z'){
-				ch = ch - 'Z' + 'A' - 1;
-			}
-			
-			message[i] = ch;
-		}
-	}
-	
-	printf("Encrypted message: %s\n", message);
-	break ;
-	
-	case 2:
-		printf("Enter a message to decrypt: ");
-	scanf("%s", message);
-	
-	  while (message[i] != '\0') {
-      if (message[i] >= 'a' && message[i] <= 'z') 
-         message[i] = message[i] - 32;
-      
-      i++;
-	      
-	  }
-	printf("Enter key: ");
-	scanf("%d", &key);
-	
-	for(i = 0; message[i] != '\0'; ++i){
-		ch = message[i];
-		
-	    if(ch >= 'A' && ch <= 'Z'){
-			ch = ch - key;
-			
-			if(ch < 'A'){
-				ch = ch + 'Z' - 'A' + 1;
-			}
-			
-			message[i] = ch;
-		}
-	}
-	
-	printf("Decrypted message: %s", message);
-	break;
-	    
-	}
-
-	}
-	return 0 ;
+char message[100], ch;
+int string[100] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+int i = 0, key, n;
+int operation, cypher;
+printf ("cypher list\n");
+printf ("1. ceaser cypher\n");
+printf ("2. substitution cypher \n");
+printf ("choose cypher:");
+scanf ("%d", &cypher);
+if (cypher == 1)
+{
+printf ("1. encrypt message\n");
+printf ("2. decrypt message\n");
+printf ("choose program: ");
+scanf ("%d", &operation);
+switch (operation)
+{
+case 1:
+printf ("Enter a message to encrypt: ");
+scanf ("%s", message);
+while (message[i] != '\0')
+{
+if (message[i] >= 'a' && message[i] <= 'z')
+message[i] = message[i] - 32;
+i++;
 }
+printf ("Enter key: ");
+scanf ("%d", &key);
+for (i = 0; message[i] != '\0'; ++i)
+{
+if (message[i] >= 'A' && message[i] <= 'Z')
+{
+message[i] = message[i] + key;
+if (message[i] > 'Z')
+{
+message[i] = message[i] - 'Z' + 'A' - 1;
+}
+}
+}
+printf ("Encrypted message: %s\n", message);
+break;
+case 2:
+printf ("Enter a message to decrypt: ");
+scanf ("%s", message);
+while (message[i] != '\0')
+{
+if (message[i] >= 'a' && message[i] <= 'z')
+message[i] = message[i] - 32;
+i++;
+}
+printf ("Enter key: ");
+scanf ("%d", &key);
+for (i = 0; message[i] != '\0'; ++i)
+{
+ch = message[i];
+if (message[i] >= 'A' && ch <= 'Z')
+{
+message[i] = message[i] - key;
+if (message[i] < 'A')
+{
+message[i] = message[i] + 'Z' - 'A' + 1;
+}
+}
+printf ("Decrypted message: %s\n", message);
+break;
+}
+}
+}
+else
+{
+printf ("1. encrypt message\n");
+printf ("2. decrypt message\n");
+printf ("choose program: ");
+scanf ("%d", &operation);
+switch (operation)
+{
+case 1:
+printf ("input what you want encrypted: ");
+scanf ("%s", message);
+while (message[i] != '\0')
+{
+if (message[i] >= 'a' && message[i] <= 'z')
+message[i] = message[i] - 32;
+i++;
+}
+for (i = 0; message[i] != '\0'; ++i)
+{
+n = message[i] - 65;
+printf ("encrypted message: %c\n", string[n]);
+}
+break;
+}
+}
+return 0;
+}
+
+
+ 
+ 
